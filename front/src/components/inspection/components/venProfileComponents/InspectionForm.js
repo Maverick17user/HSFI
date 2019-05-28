2720da56defd0b71c3f418a063e0dc40179b4a51
Maverick17user <maksim.epifanov.2000@mail.ru>
2019-05-28 18:08:10 +0200

    frontend logic is done

--- front/src/components/inspection/components/venProfileComponents/InspectionForm.js
+++ front/src/components/inspection/components/venProfileComponents/InspectionForm.js
@@ -1,73 +1,121 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
+import {initialFetch} from '../../../../actions/inspection/initialFetch'
+import {changeOSS} from '../../../../actions/inspection/changeOSS'
 
-const InspectionForm = props => {
-    const user = props.auth.user
-    const {dbVendors} = props.dbVendors
-    const selectedVendor = dbVendors.filter(ven => ven._id === props.match.params.vendorId)[0]
-    const licNum = selectedVendor.licNumber
-    const vendorName = selectedVendor.venName
-    const vendorPhoto = selectedVendor.venPhotoURL
-    const foodGroup = selectedVendor.foodGroup
-    const {dbquestions} = props.dbquestions
+class InspectionForm extends Component {
+    constructor() {
+        super()
+        this.radioHendler = this.radioHendler.bind(this)
+    }
 
-    return (
-        <div className="container inspF">
-            <h2>Inspection</h2>
+    componentDidMount() {
+        const user = this.props.auth.user
+        const {dbquestions: questions} = this.props.dbquestions
+        const {dbVendors} = this.props.dbVendors 
+        const selectedVendor = dbVendors.filter(ven => ven._id === this.props.match.params.vendorId)[0]
+        const {licNumber: licNum, venName: vendorName, venPhotoURL: vendorPhoto, foodGroup} = selectedVendor
+
+        this.props.initialFetch({
+            operatorName: user.name, 
+            date: new Date().toLocaleString().slice(0,10),
+            licNum,
+            vendorName,
+            vendorPhoto,
+            questions,
+            foodGroup,
+        })
+    }
+
+    radioHendler = e => {
+        this.props.changeOSS(e.target) 
+    }
+
+    render() {
+        const {dbquestions} = this.props.dbquestions
         
-            <form action="" className="px-4 py-3">
-                <div className="form-group">
-                    <label htmlFor="opName">Operator's Name</label>
-                    <input type="text" className="form-control" value={user.name} id="opName" readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="insDate">Inspection date</label>
-                    <input type="text" className="form-control" value={new Date().toLocaleString().slice(0,10)} id="insDate" readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="callerNatID">License number</label>
-                    <input type="text" className="form-control" value= {licNum} id="callerNatID" readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="venName">Vendor's Name</label>  
-                    <input type="text" className="form-control" value={vendorName} id="venName" readOnly />
-                </div>
-                <div className="venPicture-wrap">
-                    <img src={vendorPhoto} alt="Loading ..." width="150px" />
-                    <span>Vendor's picture</span>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="fGroup">Food group</label>
-                    <input type="text" className="form-control" value={foodGroup} id="fGroup" readOnly />
-                </div>
-                {/* <div className="form-group">
-                    <label htmlFor="GPS">GPS location</label>
-                    <input type="text" className="form-control" placeholder="GPS location" id="GPS" />
-                </div> */}
-                <div className="form-group">
-                    {dbquestions.map(question => 
-                        <div className="question-wrap">
-                            <span>{question}</span>
-                            <div className="ball">
-                                <div className="plus">+</div>
-                                <div className="minus">-</div>
+        const {
+            operatorName,
+            date,
+            licNum,
+            vendorName,
+            vendorPhoto,
+            foodGroup,
+            questionsStatus,
+            totalOSS
+        } = this.props.inspectionFormData
+
+        console.log(questionsStatus);
+        
+
+        return (
+            <div className="container inspF">
+                <h2>Inspection</h2>
+                <form action="" className="px-4 py-3">
+                    <div className="form-group">
+                        <label htmlFor="opName">Operator's Name</label>
+                        <input type="text" className="form-control" value={operatorName} id="opName" readOnly />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="insDate">Inspection date</label>
+                        <input type="text" className="form-control" value={date} id="insDate" readOnly />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="callerNatID">License number</label>
+                        <input type="text" className="form-control" value= {licNum} id="callerNatID" readOnly />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="venName">Vendor's Name</label>  
+                        <input type="text" className="form-control" value={vendorName} id="venName" readOnly />
+                    </div>
+                    <div className="venPicture-wrap">
+                        <img src={vendorPhoto} alt="Loading ..." width="150px" />
+                        <span>Vendor's picture</span>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="fGroup">Food group</label>
+                        <input type="text" className="form-control" value={foodGroup} id="fGroup" readOnly />
+                    </div>
+                    <div className="form-group questions">
+                        <label>Questions</label>
+                        {dbquestions.map((question, i) => 
+                            <div key={question} className="questions-wrap">
+                                <span className="question">{question}</span>
+                                <div className="ball">
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" name={"q" + (i+1)} type="radio" 
+                                        id="inlineRadio1" value="+" onChange={e => this.radioHendler(e)}/>
+                                        <label className="form-check-label" htmlFor="inlineRadio1">+</label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" name={"q" + (i+1)} type="radio" 
+                                        id="inlineRadio2" value="-" onChange={e => this.radioHendler(e)}/>
+                                        <label className="form-check-label" htmlFor="inlineRadio2">-</label>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    )}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="totalOSS">Total OSS</label>
-                    <input type="text" className="form-control" placeholder="Overall Safety Score OSS" id="totalOSS" readOnly />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    )
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="totalOSS">Total OSS</label>
+                        <input type="text" className="form-control" value={totalOSS}
+                        placeholder="Overall Safety Score OSS" id="totalOSS" readOnly />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        )
+    }
+    
 }
 
 const mapStateToProps = (state) => ({
     dbquestions: state.dbquestions,
+    inspectionFormData: state.inspectionFormData,
 })
 
-export default connect(mapStateToProps)(InspectionForm)
+export default connect(mapStateToProps, {
+    initialFetch,
+    changeOSS
+})(InspectionForm)
 
---
+++ front/src/reducers/inspectionFormReducer.js
@@ -0,0 +1,72 @@
+import { 
+    FETCH_IN_INSPECTION,
+    SET_TOTAL_OSS_IN_INSPECTION
+} from '../actions/types';
+
+const initialState = {
+    operatorName: '',
+    date: '',
+    licNum: '',
+    vendorName: '',
+    vendorPhoto: '',
+    foodGroup: '',
+    questionsStatus: [],
+    totalOSS: 0,
+}
+
+export default function(state = initialState, action ) {
+    switch(action.type) {
+        case FETCH_IN_INSPECTION: {
+            let questionsStatus = state.questionsStatus
+            for (let i = 0; i < action.payload.questions.length; i++) {
+                questionsStatus.push({[`q${i+1}`]: ''})
+            }
+            return Object.assign({}, state, {
+                operatorName: action.payload.operatorName,
+                date: action.payload.date,
+                licNum: action.payload.licNum,
+                vendorName: action.payload.vendorName,
+                vendorPhoto: action.payload.vendorPhoto,
+                foodGroup: action.payload.foodGroup,
+                questionsStatus
+            })
+        }
+
+        case SET_TOTAL_OSS_IN_INSPECTION: {
+            let questionsStatus = state.questionsStatus
+            let index = questionsStatus.findIndex(statusUnit => {
+                return Object.keys(statusUnit)[0] === action.target.name
+            })
+
+
+            if(index !== -1) {
+                questionsStatus[index][action.target.name] = action.target.value
+
+                let counter = 0
+                const flagArray = questionsStatus.map(questionUnit => {
+                    return questionUnit[Object.keys(questionUnit)[0]]
+                })
+
+                flagArray.forEach(flag => {
+                    if (flag !== '') {
+                        if(flag === '+') {
+                            counter += 1
+                        }
+                        else {
+                            counter -= 1
+                        }
+                    }
+                });
+
+                return Object.assign({}, state, {
+                    totalOSS: counter
+                })
+            } 
+            else {
+                return state
+            }
+        }
+        default: 
+            return state;
+    }
+}
