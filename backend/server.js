6c07266a52ceaf08b2cdf204015071f3cb7d6f79
Maverick17user <maksim.epifanov.2000@mail.ru>
2019-06-10 11:13:20 +0200

    heroku staff

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,20 @@ const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../front/build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../front/build', 'index.html'));
+});
+
 app.use(passport.initialize());
 require('./passport')(passport);
 
